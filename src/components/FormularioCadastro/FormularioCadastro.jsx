cceae4d2767f25bb9bf84fb07571f175e6089ff0
Camila Brognara <cbrognara@gmail.com>
2022-01-20 13:21:15 -0300

    criadas funcoes de useState

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,49 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { FormControlLabel, Switch, TextField, Button } from '@material-ui/core';
 
 
 function FormularioCadastro() {
-  let nome = ''
+  const [nome, setNome] = useState('')
+  const [sobrenome, setSobrenome] = useState('')
+  const [cpf, setCPF] = useState('')
+
   return (
   <form onSubmit={(event) => {
     event.preventDefault()
+    console.log(nome, sobrenome, cpf)
   }}>
     <TextField
     value={nome}
     onChange={(event) => {
-      nome = event.target.value
-    console.log(event.target.value)
-
+      setNome(event.target.value)
     }} 
     id="nome" 
     label="Nome" 
-    variant="outlined" 
+    variant="outlined"
     fullWidth 
     margin='normal'
     />
+
     <TextField 
+    value={sobrenome}
+    onChange={(event) => {
+      setSobrenome(event.target.value)
+    }}
     id="sobrenome" 
     label="Sobrenome" 
     variant="outlined" 
     fullWidth 
     margin='normal'/>
+
     <TextField 
+    value={cpf}
+    onChange={(event) =>{
+      setCPF(event.target.value)
+    }}
     id="cpf" 
     label="CPF" 
     variant="outlined" 
     fullWidth 
     margin='normal'/>
+
     <FormControlLabel 
     control={<Switch 
     defaultChecked 
     color='primary'/>} 
     label="Promoções"  />
+
     <FormControlLabel 
     control={<Switch 
     defaultChecked 
     color='primary' />} 
     label="Novidades" />
+
     <Button 
     type='submit' 
     variant='contained'
